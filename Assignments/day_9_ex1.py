686da45102d9e902a8557642ba6c1b190b6829df
nuradilahrozman <nuradilahrozman@gmail.com>
2025-10-16 12:11:32 +0800

    exercise-01

---
+++ Assignments/day_9_ex1.py
@@ -0,0 +1,6 @@
+def findMax(num1,num2,num3):
+    return max(num1,num2,num3)
+num1=int(input('Please Enter First Number : '))
+num2=int(input('Please Enter Second Number : '))
+num3=int(input('Please Enter Third Number : '))
+print(f'Maximum from {num1},{num2},{num3} is : ',max(num1,num2,num3))
